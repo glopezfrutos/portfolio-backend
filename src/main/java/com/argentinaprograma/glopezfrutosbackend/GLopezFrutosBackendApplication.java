28902130fbba0682986009349ff24a99e3784613
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-06-18 01:45:19 -0300

    initial commit

---
+++ src/main/java/com/argentinaprograma/glopezfrutosbackend/GLopezFrutosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.argentinaprograma.glopezfrutosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GLopezFrutosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GLopezFrutosBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/argentinaprograma/glopezfrutosbackend/GLopezFrutosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argentinaprograma.glopezfrutosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GLopezFrutosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
