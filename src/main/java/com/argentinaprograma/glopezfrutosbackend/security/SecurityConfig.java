ee0738b304ebab332e29da6159e80e0c94451a04
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-06-21 12:11:13 -0300

    ADD --- CustomAuthorizationFilter

--- src/main/java/com/argentinaprograma/glopezfrutosbackend/security/SecurityConfig.java
+++ src/main/java/com/argentinaprograma/glopezfrutosbackend/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.argentinaprograma.glopezfrutosbackend.security;
 
 import com.argentinaprograma.glopezfrutosbackend.filter.CustomAuthenticationFilter;
+import com.argentinaprograma.glopezfrutosbackend.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -40,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(POST, "/api/v1/user/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
